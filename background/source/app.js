5ed6d86dc81308c1c9e4d3e68fb66bb7fafc948f
DESKTOP-PD2MENK\Jumasultanov <jumasultanov@rambler.ru>
2020-12-16 16:20:46 +0500

    add background connector scripts comments

--- background/source/app.js
+++ background/source/app.js
@@ -1,5 +1,5 @@
 import App from './core/App.js';
-
+//Создаем объект приложения
 let app = App.getInstance();
-
+//Инициализируем ее
 app.init();
--- background/source/config.js
+++ background/source/config.js
@@ -1,3 +1,8 @@
+/**
+ * Файл конфигуации для приложения
+ */
+
+//Конфиг с данными по соединению с сервером
 export let SERVICE_CONFIG = {
     HOST: 'dev-tools.local',
     API_ROOT: 'http://dev-tools.local/extension',
