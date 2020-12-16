afe067f37b6c9404c65739c41efc1a89d544535b
DESKTOP-PD2MENK\Jumasultanov <jumasultanov@rambler.ru>
2020-12-16 16:33:36 +0500

    add background core comments

--- background/source/core/App.js
+++ background/source/core/App.js
@@ -2,18 +2,27 @@ import ServiceController from '../controller/ServiceController.js';
 import ContentController from '../controller/ContentController.js';
 import PopupController from '../controller/PopupController.js';
 
+/**
+ * Класс приложения
+ */
 class App {
 
     static obj;
 
+    //Данные пользователя
     user;
+    //Комбинации клавиш
     keys;
 
     constructor() {
         
     }
 
+    /**
+     * Инициализация
+     */
     init() {
+        //Подключение контроллера обмена данными с сервером
         ServiceController.connect()
             .then(() => {
 
@@ -21,10 +30,15 @@ class App {
             .catch(err => {
                 console.error(err.get());
             });
+        //Подключение контроллера для работы со страницей
         ContentController.connect();
+        //Подключение контроллера для работы со попапом расширения
         PopupController.connect();
     }
 
+    /**
+     * Возвращает единственный объект приложения по принципу Singleton
+     */
     static getInstance() {
         if (!this.obj) this.obj = new this();
         return this.obj;
--- background/source/core/Fetch.js
+++ background/source/core/Fetch.js
@@ -1,7 +1,11 @@
 import FetchException from '../exception/FetchException.js';
 
+/**
+ * Класс для работы с запросами
+ */
 class Fetch {
 
+    //Данные запроса
     url;
     headers;
     method;
@@ -13,18 +17,31 @@ class Fetch {
         this.headers = headers;
     }
 
+    /**
+     * GET-запрос
+     * @return {Promise}
+     */
     get() {
         this.method = 'GET';
         return this.run();
     }
 
+    /**
+     * POST-запрос
+     * @return {Promise}
+     */
     post() {
         this.method = 'POST';
         return this.run();
     }
 
+    /**
+     * Запрос
+     * @return {Promise}
+     */
     run() {
         return new Promise((resolve, reject) => {
+            //Собираем данные по запросу
             let params = {
                 headers: Object.assign({
                     'Extension-Request': true
@@ -32,8 +49,10 @@ class Fetch {
                 method: this.method,
                 body: Fetch.objectToFormData(this.data)
             };
+            //Делаем fetch-запрос
             fetch(this.url, params)
                 .then(res => {
+                    //Проверяем данные и статус
                     if (res.status==200) {
                         let contentType = res.headers.get("content-type");
                         if (contentType && contentType.indexOf("application/json") !== -1) {
@@ -44,6 +63,7 @@ class Fetch {
                     } else throw new FetchException(FetchException.RESPONSE_CODE_ERROR, res.status);
                 })
                 .then(data => {
+                    //Проверяем данные
                     if (typeof data == "string") throw new FetchException(FetchException.RESPONSE_TEXT, data);
                     let err = '';
                     if (data) {
@@ -53,21 +73,29 @@ class Fetch {
                     throw new FetchException(FetchException.RESPONSE_ERROR, err || 'Пустое значение');
                 })
                 .catch(err => {
+                    //Обрабатываем выброшенные исключения
                     if (err instanceof FetchException) reject(err);
                     else reject(new FetchException(FetchException.FAILED));
                 });
         });
     }
     
+    /**
+     * Преобразование объекта в объект FormData
+     * @param {Object} obj Объект, который надо преобразовать
+     * @param {FormData|undefined} form Объекта формы, в который надо записать
+     * @param {String|undefined} namespace Родительское название
+     * @return {FormData}
+     */
     static objectToFormData(obj, form, namespace) {
         var fd = form || new FormData();
         var formKey;
         for(var property in obj) {
             if(obj.hasOwnProperty(property)) {
-                if(namespace) formKey = namespace + '[' + property + ']';
+                if (namespace) formKey = namespace + '[' + property + ']';
                 else formKey = property;
                 var append = true;
-                if(typeof obj[property] === 'object' && !(obj[property] instanceof File)) append = false;
+                if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) append = false;
                 if (obj[property] instanceof Array && !obj[property].length) append = true;
                 if (append) fd.append(formKey, obj[property]);
                 else this.objectToFormData(obj[property], fd, formKey);
